695c47e050484dc858edc8044e08610f184ac967
Ezequielp19 <135364525+eze4556@users.noreply.github.com>
2026-04-16 13:45:03 -0300

    fix(build): conditional output for Vercel and new build:android script

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: process.env.NODE_ENV === 'production' ? 'export' : undefined,
+  output: process.env.VERCEL ? undefined : 'export',
   trailingSlash: true,
   images: {
     unoptimized: true
